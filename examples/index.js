cc17cb36cfcf3b59e6bb5abf61181b295e71885c
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-01-09 21:08:43 +0100

    [*] remove class component
    [*] remove examples

--- examples/index.js
+++ examples/index.js
@@ -1,16 +1,3 @@
-import CompWithProps from './CompWithProps';
-import CompWithSass from './CompWithSass';
-import NativeCompWithProps from './NativeCompWithProps';
-import NormalComp from './NormalComp';
-import NormalNativeComp from './NormalNativeComp';
-import TypescriptComp from './TypescriptComp';
-import TypescriptNativeComp from './TypescriptNativeComp';
-export {
-  CompWithProps,
-  CompWithSass,
-  NativeCompWithProps,
-  NormalComp,
-  NormalNativeComp,
-  TypescriptComp,
-  TypescriptNativeComp,
-};
+import CompWithProps from './CompWithProps'
+import CompWithSass from './CompWithSass'
+export { CompWithProps, CompWithSass }
